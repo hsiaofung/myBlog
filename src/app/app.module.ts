24b0515ff2a3fddaf9d2423f7b6798678dec6fab
hsiaofung <hsiaofung.chou@gmail.com>
2021-04-30 16:19:20 +0800

    add catalog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule
