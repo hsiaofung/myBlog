f9ef3abc9711eac169126d06557fee8fcb72d80c
hsiaofung <hsiaofung.chou@gmail.com>
2021-05-04 19:05:48 +0800

    add homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { CatalogComponent } from './catalog/catalog.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    CatalogComponent
+    CatalogComponent,
+    PostListComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
